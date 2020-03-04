31f3d248e0f4d1aa3874d5cde320276c7cb8bc9f
LuisxDv23 <61606728+LuisxDv23@users.noreply.github.com>
2020-03-03 21:08:12 -0600

    Second activity created

---
+++ Simple AI.py
@@ -0,0 +1,21 @@
+Name="Luis Angel"
+Last_name="Maldonado Avelino"
+Age="18 years"
+Number="5586255674"
+ID="Id 193110905"
+Gamertag="LuisXdv23"
+sep='*****************************'
+print('{sep}')
+print('Hi {Name}')
+print('{sep}')
+print('Last Name:{Last_name}')
+print('{sep}')
+print('You are Age:{Age} years old')
+print('{sep}')
+print('Number"{Number}')
+print('{sep}')
+print('ID:{ID}')
+print('{sep}')
+print('Gamertag:{Gamertag}')
+print('{sep}')
+time.sleep(5)
